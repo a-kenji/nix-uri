1995d9ac2bc952861dea963942066df7bccbf066
a-kenji <aks.kenji@protonmail.com>
2024-11-01 15:33:34 +0100

    nix/rust: Improve toolchain naming

--- nix/rust.nix
+++ nix/rust.nix
@@ -5,11 +5,11 @@
 }:
 let
   RUST_TOOLCHAIN = self + "/rust-toolchain.toml";
-  RUSTFMT_TOOLCHAIN = self + "/.rust-nightly-toolchain.toml";
+  RUST_NIGHTLY_TOOLCHAIN = self + "/.rust-nightly-toolchain.toml";
 in
 rec {
   rustToolchainTOML = pkgs.rust-bin.fromRustupToolchainFile RUST_TOOLCHAIN;
-  rustNightlyToolchainTOML = pkgs.rust-bin.fromRustupToolchainFile RUSTFMT_TOOLCHAIN;
+  rustNightlyToolchainTOML = pkgs.rust-bin.fromRustupToolchainFile RUST_NIGHTLY_TOOLCHAIN;
   rustToolchainDevTOML = rustToolchainTOML.override {
     extensions = [
       "clippy"
