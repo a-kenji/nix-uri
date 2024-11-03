8777d571eb3091a403f329cce8a0f3ee1d81ab18
a-kenji <aks.kenji@protonmail.com>
2024-11-03 13:54:38 +0100

    nix/rust: Use stable rust version for `rustfmt`
    Use stable rust version for `rustfmt`, only the fuzz targets depend on a
    nightly toolchain now.

--- nix/rust.nix
+++ nix/rust.nix
@@ -5,11 +5,10 @@
 }:
 let
   RUST_TOOLCHAIN = self + "/rust-toolchain.toml";
-  RUST_NIGHTLY_TOOLCHAIN = self + "/.rust-nightly-toolchain.toml";
 in
 rec {
   rustToolchainTOML = pkgs.rust-bin.fromRustupToolchainFile RUST_TOOLCHAIN;
-  rustNightlyToolchainTOML = pkgs.rust-bin.fromRustupToolchainFile RUST_NIGHTLY_TOOLCHAIN;
+  rustLatestNightlyToolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default);
   rustToolchainDevTOML = rustToolchainTOML.override {
     extensions = [
       "clippy"
